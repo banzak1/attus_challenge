b6fff0faf35059c809f11b5071158e6f5aa7ee4c
Banzak <leosantanabr@gmail.com>
2026-06-17 11:12:23 -0300

    chore: configura Jest com jest-preset-angular e remove Karma/Jasmine

---
+++ frontend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFiles: ['<rootDir>/src/setup-jest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@environments/(.*)': '<rootDir>/src/environments/$1',
+  },
+};
