cbd65103d8992923b02903e9ffb08b6d9b953e97
Banzak <leosantanabr@gmail.com>
2026-06-17 11:18:12 -0300

    chore: configura jsdom e testEnvironment no Jest para Angular

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFiles: ['<rootDir>/src/setup-jest.ts'],
+  testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
   moduleNameMapper: {
     '@app/(.*)': '<rootDir>/src/app/$1',
